000679aae97660edebefe3f526c603bac4dd3429
Jose Maria Farfan <magoalakai@gmail.com>
2021-01-28 12:58:38 +0100

    Side Navbar, efecto sombra y contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactoUsuario } from './../models/contacto.usuario';
 import { Router } from '@angular/router';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import{ init } from 'emailjs-com';
+init("user_xWHAsizuYn8plxh4cTDy6");
 
 @Component({
   selector: 'app-contact',
@@ -21,10 +24,14 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(form){
-    console.log(this.contactoUsuario);
-    this.contactosUsuarios.push(this.contactoUsuario);
-    console.log(this.contactosUsuarios);
+  onSubmit(e: Event, form) {
+    emailjs.sendForm('gmail', 'template_acs7is5', e.target as HTMLFormElement, 'user_xWHAsizuYn8plxh4cTDy6')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+
     form.reset();
     this.enviarAlert();
   }
@@ -34,6 +41,7 @@ export class ContactComponent implements OnInit {
     editor.classList.remove('d-none');
     setTimeout(()=> editor.classList.add('d-none'), 4000);
   }
+
 
 
 }
